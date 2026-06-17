4d20e872754e3b7080316ac5e760356ab7a5d67b
davidyeat <david.yeat123@gmail.com>
2026-06-17 13:20:18 +0700

    project structure

---
+++ backend/src/models/scholarshipMockData.js
@@ -0,0 +1,32 @@
+// Scholarships mock data...
+
+const scholarships = [
+    {
+        id: 1,
+        title: "",
+        destination: "",
+        type: "",
+        degree:"",
+        major:"",
+        description:"",
+        benefits:"",
+        eligibility:"",
+        deadline:"",
+        link:"",
+        
+    },
+    {
+        id: 2,
+        title: "",
+        destination: "",
+        type: "",
+        degree:"",
+        major:"",
+        description:"",
+        benefits:"",
+        eligibility:"",
+        deadline:"",
+        link:"",
+        
+    }
+];
